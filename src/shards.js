80ef1b9c79d5774585910cc77e65a4919994bf9d
SharifPoetra <sharifpoetra1@gmail.com>
2019-07-26 00:23:42 +0000

    Emm

--- src/shards.js
+++ src/shards.js
@@ -1,5 +1,5 @@
-const Logger = require("./util/console");
 const { ShardingManager } = require("discord.js");
+const Logger = require("./util/console");
 const shards = new ShardingManager("./src/main.js", {
     totalShards: "auto",
     token: process.env.DEV ? process.env.DEV_TOKEN : process.env.TOKEN
