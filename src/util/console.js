9babbc49d3b66a0420486071013bcd69e933e846
BillyAddlers <finnsonalca123@gmail.com>
2019-07-26 07:27:57 +0700

    Changing stacktracing method

--- src/util/console.js
+++ src/util/console.js
@@ -1,14 +1,15 @@
 const chalk = require("chalk");
 
 class Logger {
-	
-	static info(reason) {
-		return console.info(`${chalk.red(`[${new Date().toString().split(" ", 5).join(" ")}]`)} ${chalk.blue(" INFO: ")} ${reason}`);
-	}
-	
-	static error(reason) {
-		return console.error(`${chalk.red(`[${new Date().toString().split(" ", 5).join(" ")}]`)} ${chalk.red(" ERROR: ")} ${reason}`);
-	}
+
+    static info(data) {
+        return console.info(`${chalk.red(`[${new Date().toString().split(" ", 5).join(" ")}]`)} [${chalk.magenta(data.name)}||${chalk.red(data.pid)}] ${chalk.bgGreen(" INFO : ")} ${data.message}`); // eslint-disable-line
+    }
+
+    static debug(data) {
+        return console.debug(`${chalk.red(`[${new Date().toString().split(" ", 5).join(" ")}]`)} [${chalk.magenta(data.name)}||${chalk.red(data.pid)}] ${chalk.bgBlue(" DEBUG: ")} ${data.message}`); // eslint-disable-line
+    }
+
 }
 
 module.exports = Logger;
