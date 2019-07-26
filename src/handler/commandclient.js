45412f9a0e721cd24397aaad839f1421a5e8b730
dwicao <lutfiandwica@gmail.com>
2019-07-26 10:34:59 +0700

    Fix: json syntax errors

--- src/handler/commandclient.js
+++ src/handler/commandclient.js
@@ -6,42 +6,40 @@ const TreeMap = require("../util/treemap");
 const LoggingFactory = require("./loggingfactory");
 
 module.exports = class CommandClient extends Japanisme {
+  constructor(options, djsopt) {
+    super(djsopt);
+    this.path = options.path;
+    this.commands = new TreeMap();
+    this.aliases = new TreeMap();
+    this.cooldowns = new TreeMap();
+  }
 
-    constructor(options, djsopt) {
-        super(djsopt);
-        this.path = options.path;
-        this.commands = new TreeMap();
-        this.aliases = new TreeMap();
-        this.cooldowns = new TreeMap();
-    }
+  /**
+   * @getter
+   */
+  get Commands() {
+    return this.commands;
+  }
+  get Aliases() {
+    return this.aliases;
+  }
 
-    /**
-	 * @getter
-	 */
-    get Commands() {
-        return this.commands;
-    }
-    get Aliases() {
-        return this.aliases;
-    }
-
-    build() {
-        const logger = new LoggingFactory(this);
-        const modules = fs.readdirSync(join("./src", this.path));
-        for (const Module of modules) {
-            logger.print(`${Module} Loaded!`);
-            const commands = fs.readdirSync(join("./src", this.path, Module));
-            for (const file of commands) {
-                const Command = require(`../${this.path}/${Module}/${file}`);
-                const resolved = new Command();
-                resolved.category = Module;
-                logger.print(`Resolving command ${resolved.name}`);
-                this.commands.set(resolved.name.toLowerCase(), resolved);
-                for (const alias of resolved.aliases) {
-                    this.aliases.set(alias.toLowerCase(), resolved.name.toLowerCase());
-                }
-            }
+  build() {
+    const logger = new LoggingFactory(this);
+    const modules = fs.readdirSync(join("./src", this.path));
+    for (const Module of modules) {
+      logger.print(`${Module} Loaded!`);
+      const commands = fs.readdirSync(join("./src", this.path, Module));
+      for (const file of commands) {
+        const Command = require(`../${this.path}/${Module}/${file}`);
+        const resolved = new Command();
+        resolved.category = Module;
+        logger.print(`Resolving command ${resolved.name}`);
+        this.commands.set(resolved.name.toLowerCase(), resolved);
+        for (const alias of resolved.aliases) {
+          this.aliases.set(alias.toLowerCase(), resolved.name.toLowerCase());
         }
+      }
     }
-
+  }
 };
